a0e21f8c6c69ef053623d138ddf43d8a14d11087
Lennart Wilde <lennart.wilde@rwth-aachen.de>
2022-05-04 00:40:07 +0200

    Numerov v2
    Signed-off-by: Lennart Wilde <lennart.wilde@rwth-aachen.de>

--- Theo/Theo_3/exercise4_numerov.py
+++ Theo/Theo_3/exercise4_numerov.py
@@ -4,9 +4,12 @@
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 m = 1
 h = 1
+a = 1
+
 
 # Numerov Algorithmus
 def numerov(eps, v, f0, f0p, step=0.001, thres=10.):
@@ -29,33 +32,32 @@ def numerov(eps, v, f0, f0p, step=0.001, thres=10.):
 
     # first two points
     x = [0., step]
-    f = [f0, f0 + f0p*step]
+    f = [f0, f0 + f0p * step]
     xjp1 = 0
-    
+
     while abs(f[-1]) < thres:
         # previous two values
         xjm1 = x[-2]
         gjm1 = eps - v(xjm1)
-        hjm1 = 1. + gjm1*step**2/12.
+        hjm1 = 1. + gjm1 * step ** 2 / 12.
         fjm1 = f[-2]
 
         xj = x[-1]
         gj = eps - v(xj)
-        hj = 1. + gj*step**2/12.
+        hj = 1. + gj * step ** 2 / 12.
         fj = f[-1]
 
         # calculate next point
         xjp1 = xj + step
         gjp1 = eps - v(xjp1)
-        hjp1 =1. + gjp1*step**2/12.
-        fjp1 = ((12. - 10.*hj)*fj - hjm1*fjm1 )/hjp1
-
+        hjp1 = 1. + gjp1 * step ** 2 / 12.
+        fjp1 = ((12. - 10. * hj) * fj - hjm1 * fjm1) / hjp1
 
         # add the new point
         x.append(xjp1)
         f.append(fjp1)
 
-    return (x,f)
+    return x, f
 
 
 def numerov_g(eps, v, step=0.001, thres=10.):
@@ -95,78 +97,29 @@ def numerov_u(eps, v, step=0.001, thres=10.):
     return numerov(eps, v, 0., 1., step, thres)
 
 
-# mittlere potentielle Energie
-def v_av(eps, v, x, f):
-    """calculates the average potential energy
-            ∫ |f(x)|^2 v(x) dx /  ∫ |f(x)|^2 dx
-
-    input:
-        eps:    float
-        v:      function float -> float
-        x,f:    return values of numerov
-
-
-    output:
-        float:  average potential energy
-    """
-
-    vres = 0.
-    norm = 0.
-
-    for i in range(len(x)):
-        vres += f[i]**2 * v(x[i])
-        norm += f[i]**2
-
-        # inside classical forbidden region
-        if v(x[i]) > eps:
-            # if abs(wavefunction) increases
-            if abs(f[i]) > abs(f[i-1]):
-                break
-
-    return vres/norm
-
-
-
-def num_derivative(x, f, delta_x = 0.05):
-    return (f(x) - f(x+delta_x))/delta_x
-
 def v(x):
-    return np.array(x)**4
+    return a * np.array(x) ** 4
 
-def func(x):
-    l, _ = numerov_g(x, lambda x : 2*m/h**2 * v(x))
-    return l[-1]
 
 # hier kommt das eigentliche Programm, dass editiert werden muss
 def main():
     # Energie
     eps = 0
-    le = [2.5]
-    delta_eps = 0.1
+    x = []
 
     eps_s_g = []
-    len_s_g=[]
-    peakind = [0]
-    c = 0.01
+    len_s_g = []
 
-    while (len(peakind) < 10):
+    energies = np.linspace(0, 100, 1000)
+
+    for eps in tqdm(energies):
         # gerade Wellenfunktion bestimmen
-        le, f = numerov_g(eps, lambda x : 2*m/h**2 * v(x))
-        df = abs(num_derivative(eps, func))
-
-        if df != 0.0:
-            if c/df < 10e-7:
-                delta_eps = 0.01
-            else:
-                delta_eps = c/df
-
-        eps += delta_eps
+        x, f = numerov_g(eps, lambda x: 2 * m / h ** 2 * v(x))
 
         eps_s_g.append(eps)
-        len_s_g.append(le[-1])
-        if eps > 400:
-            break
-        peakind, _ = signal.find_peaks(len_s_g)
+        len_s_g.append(x[-1])
+
+    peakind, _ = signal.find_peaks(len_s_g)
 
     eps_s_g_sub = []
     len_s_g_sub = []
@@ -176,37 +129,23 @@ def main():
         len_s_g_sub.append(len_s_g[ind])
 
     E_val_g = np.asarray(eps_s_g_sub)
-    E_val_g *= h**2/(2*m)
     print(E_val_g)
 
     # ungerade Funktionen
     eps = 0
-    le = [2.5]
-    delta_eps = 0.1
+    x = []
 
     eps_s_u = []
-    len_s_u=[]
-    peakind = [0]
-    c = 0.01
+    len_s_u = []
 
-    while (len(peakind) < 10):
+    for eps in tqdm(energies):
         # gerade Wellenfunktion bestimmen
-        le, f = numerov_u(eps, lambda x : 2*m/h**2 * v(x))
-        df = abs(num_derivative(eps, func))
-
-        if df != 0.0:
-            if c/df < 10e-7:
-                delta_eps = 0.01
-            else:
-                delta_eps = c/df
-
-        eps += delta_eps
+        x, f = numerov_u(eps, lambda x: 2 * m / h ** 2 * v(x))
 
         eps_s_u.append(eps)
-        len_s_u.append(le[-1])
-        if eps > 400:
-            break
-        peakind, _ = signal.find_peaks(len_s_u)
+        len_s_u.append(x[-1])
+
+    peakind, _ = signal.find_peaks(len_s_u)
 
     eps_s_u_sub = []
     len_s_u_sub = []
@@ -216,22 +155,20 @@ def main():
         len_s_u_sub.append(len_s_u[ind])
 
     E_val_u = np.asarray(eps_s_u_sub)
-    E_val_u *= h**2/(2*m)
     print(E_val_u)
 
     fig = plt.figure(figsize=(7, 7))
 
-    plt.plot(eps_s_g, len_s_g, label = "Convergence Radius")
-    plt.plot(eps_s_g_sub, len_s_g_sub, marker = "o", linewidth = 0, label = "Peak")
+    plt.plot(eps_s_g, len_s_g, label="Convergence Radius")
+    plt.plot(eps_s_g_sub, len_s_g_sub, marker="o", linewidth=0, label="Peak")
 
-    plt.plot(eps_s_u, len_s_u, label = "Convergence Radius (uneven)")
-    plt.plot(eps_s_u_sub, len_s_u_sub, marker = "o", linewidth = 0, label = "Peak")
+    plt.plot(eps_s_u, len_s_u, label="Convergence Radius (uneven)")
+    plt.plot(eps_s_u_sub, len_s_u_sub, marker="o", linewidth=0, label="Peak")
 
     plt.title("Finding Converging Eigenfunctions for a given Energy")
 
     plt.ylabel("Covergence Radius")
     plt.xlabel("Energy")
-
 
     plt.savefig("Convergence.eps")
     plt.show()
@@ -240,13 +177,13 @@ def main():
 
     for e in list(E_val_g):
         e_r = e
-        le_g, f_g = numerov_g(2*m/h**2*e_r, lambda x : 2*m/h**2 * v(x))
+        le_g, f_g = numerov_g(e_r, lambda x: 2 * m / h ** 2 * v(x))
 
         fig = plt.figure(figsize=(7, 7))
 
-        plt.plot(np.array(le_g), 100*np.array(f_g) + e_r, label = "Wave Function")
-        plt.plot(le_g, v(le_g), label = "Potential")
-        plt.axhline(e, color="g", label = "Energy of Particle")
+        plt.plot(np.array(le_g), 100 * np.array(f_g) + e_r, label="Wave Function")
+        plt.plot(le_g, v(le_g), label="Potential")
+        plt.axhline(e, color="g", label="Energy of Particle")
         plt.title(r"E_{0}: {1:.3f}".format(list(E_val_g).index(e), e_r))
         plt.xlim(0, 3.5)
         plt.ylim(-100, 300)
@@ -255,22 +192,20 @@ def main():
         plt.ylabel("Energy")
         plt.xlabel("Position")
 
-
         plt.savefig("WF_E{0}.eps".format(list(E_val_g).index(e)))
         plt.show()
-        print('Erwartungswert v(x):', v_av(e, v, le_g, f_g))
 
     # plot ungerade
 
     for e in list(E_val_u):
         e_r = e
-        le_u, f_u = numerov_u(2*m/h**2*e_r, lambda x : 2*m/h**2 * v(x))
+        le_u, f_u = numerov_u(e_r, lambda x: 2 * m / h ** 2 * v(x))
 
         fig = plt.figure(figsize=(7, 7))
 
-        plt.plot(np.array(le_u), 100*np.array(f_u) + e_r, label = "Wave Function")
-        plt.plot(le_u, v(le_u), label = "Potential")
-        plt.axhline(e, color="g", label = "Energy of Particle")
+        plt.plot(np.array(le_u), 100 * np.array(f_u) + e_r, label="Wave Function")
+        plt.plot(le_u, v(le_u), label="Potential")
+        plt.axhline(e, color="g", label="Energy of Particle")
         plt.title(r"E_{0}: {1:.3f}".format(list(E_val_u).index(e), e_r))
         plt.xlim(0, 3.5)
         plt.ylim(-100, 300)
@@ -279,13 +214,8 @@ def main():
         plt.ylabel("Energy")
         plt.xlabel("Position")
 
-
         plt.savefig("WF_E{0}.eps".format(list(E_val_u).index(e)))
         plt.show()
-        print('Erwartungswert v(x):', v_av(e, v, le_u, f_u))
-
-
-    # mittlere potentielle Energie
 
 
 if __name__ == '__main__':
